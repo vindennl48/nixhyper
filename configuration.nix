6182532ca3a1c03ebf63cf5c1e59f5b01010a890
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2024-07-20 18:42:05 -0400

    Initial NixOS Hypervisor setup
    This setup successfully does GPU passthrough and looking-glass.

---
+++ configuration.nix
@@ -0,0 +1,427 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+################################################################################
+## ENVIRONMENT #################################################################
+################################################################################
+  environment = {
+    systemPackages = with pkgs; [
+      wget
+      git
+      less
+      gcc
+      neovim nodejs yarn xclip
+      spice-vdagent
+      alacritty
+      turbovnc
+      pciutils
+      htop
+      virt-manager virt-viewer spice spice-gtk spice-protocol win-virtio win-spice
+      gnome.adwaita-icon-theme
+      looking-glass-client
+    ];
+
+    # Environment Variables
+    variables = {
+      # EXAMPLE_VAR = true;
+    };
+  };
+
+################################################################################
+## PROGRAMS ####################################################################
+################################################################################
+  programs = {
+    dconf.enable = true; # system management tool
+    firefox.enable = true; # Install firefox.
+    zsh.enable = true; # Install zsh
+
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # mtr.enable = true;
+    # gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
+  };
+
+  ## libvirt and virt-manager
+  # programs.virt-manager.enable = true; # Install virt-manager
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+	ovmf.enable = true;
+	ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
+  services.spice-vdagentd.enable = true;
+
+################################################################################
+## FONTS #######################################################################
+################################################################################
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "RobotoMono" "FiraCode" "DroidSansMono" ]; })
+  ];
+
+################################################################################
+## USERS #######################################################################
+################################################################################
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.mitch = {
+    isNormalUser = true;
+    description = "mitch";
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    #  thunderbird
+    ];
+    shell = pkgs.zsh;
+  };
+
+################################################################################
+## SERVICES ####################################################################
+################################################################################
+  services = {
+    xserver = {
+      # Enable the X11 windowing system.
+      # You can disable this if you're only using the Wayland session.
+      enable = true;
+
+      # Enable touchpad support (enabled default in most desktopManager).
+      # services.xserver.libinput.enable = true;
+
+      xkb.layout = "us";
+      xkb.variant = "";
+
+      ## Login screens: none will default to nixos login
+      displayManager.sddm.enable = true; # use with plasma
+      # displayManager.gdm.enable = true;
+
+      ## Desktop Environment
+      desktopManager.plasma6.enable = true;
+      # desktopManager.gnome.enable = true;
+      # desktopManager.cinnamon.enable = true;
+      # windowManager.dwm.enable = true;
+    };
+
+    # ## Login Screen
+    # greetd = {
+    #   enable = true;
+    #   settings = {
+    #     default_session = {
+    #       command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+    #       user = "greeter";
+    #     };
+    #   };
+    # };
+
+    openssh = {
+      enable = true;
+      settings = {
+        PermitRootLogin = "no";
+        PasswordAuthentication = true;
+      };
+      ports = [ 22 ];
+    };
+
+    ## When running as VM / KVM / QEMU / VBOX
+    # Clipboard
+    # spice-vdagentd.enable = true;
+    ## Time sync and scripting
+    # qemuGuest.enable = true;
+    ## VirtFS alternative for directory sharing
+    # spice-webdavd.enable = true;
+
+    # Enable CUPS to print documents.
+    # printing.enable = true;
+  };
+
+  systemd = {
+    targets = {
+      sleep.enable = false;
+      suspend.enable = false;
+      hibernate.enable = false;
+      hybrid-sleep.enable = false;
+    };
+
+    tmpfiles.rules = [
+      "f /dev/shm/looking-glass 0660 mitch qemu-libvirtd -"
+    ];
+  };
+
+  ## PIPEWIRE
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+################################################################################
+## NETWORKING ##################################################################
+################################################################################
+  networking = {
+    # Enable networking
+    networkmanager.enable = true;
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    hostName = "nixos"; # Define your hostname.
+
+    # Configure network proxy if necessary
+    # proxy.default = "http://user:password@proxy:port/";
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Open ports in the firewall.
+    firewall.allowedTCPPorts = [ 22 8000 5901 ];
+    # firewall.allowedUDPPorts = [ ... ];
+    # firewall.enable = false; # disable the firewall altogether.
+  };
+
+################################################################################
+## BOOT ########################################################################
+################################################################################
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = [
+      "intel_iommu=on"
+      "iommu=pt"
+      "pcie_aspm=off"
+      ''vfio-pci.ids="10de:2705,10de:22bb"''
+  ];
+  boot.kernelModules = [ "kvm-intel" ];
+
+  boot.initrd.availableKernelModules = [ "nvidiafb" "vfio-pci" ];
+  boot.initrd.preDeviceCommands = ''
+    DEVS="0000:01:00.0 0000:01:00.1"
+    for DEV in $DEVS; do
+      echo "vfio-pci" > /sys/bus/pci/devices/$DEV/driver_override
+    done
+    modprobe -i vfio-pci
+  '';
+
+################################################################################
+## LOCALE ######################################################################
+################################################################################
+  i18n = {
+    # Select internationalisation properties.
+    defaultLocale = "en_US.UTF-8";
+
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+  };
+
+################################################################################
+## SYSTEM ######################################################################
+################################################################################
+  # Testing out build scripts
+  # system.activationScripts.script.text = ''
+  #   # source ${config.system.build.setEnvironment} # this crashes the kernel on boot
+
+  #   # since this will be run as root, need this
+  #   setPermissions() {
+  #     argpath=$1
+  #     if [ -d $argpath ]; then
+  #       # folder
+  #       chown -R mitch:users $argpath
+  #       chmod -R 755 $argpath
+  #     elif [ -f $argpath ]; then
+  #       # file
+  #       chown mitch:users $argpath
+  #       chmod 644 $argpath
+  #     fi
+  #   }
+
+  #   mkdir -p /home/mitch/bin
+
+  #   dottest="/home/mitch/bin/dotfiles"
+  #   if [ ! -e $dottest ]; then
+  #     if ${pkgs.git}/bin/git clone https://github.com/vindennl48/dotfiles $dottest; then
+  #       # git clone https://github.com/vindennl48/dotfiles $dottest
+  #       echo "----> Cloned Dotfiles; press enter to continue" && read
+  #       setPermissions $dottest
+  #     else
+  #       echo "----> Failed to clone dotfiles; press enter to continue" && read
+  #     fi
+  #   fi
+  # '';
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # allow packages like Nvidia drivers / steam / etc.
+  nixpkgs.config.allowUnfree = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
+
+# # Edit this configuration file to define what should be installed on
+# # your system.  Help is available in the configuration.nix(5) man page
+# # and in the NixOS manual (accessible by running ‘nixos-help’).
+# 
+# { config, pkgs, ... }:
+# 
+# {
+#   imports =
+#     [ # Include the results of the hardware scan.
+#       ./hardware-configuration.nix
+#     ];
+# 
+#   # Bootloader.
+#   boot.loader.systemd-boot.enable = true;
+#   boot.loader.efi.canTouchEfiVariables = true;
+# 
+#   networking.hostName = "nixos"; # Define your hostname.
+#   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+# 
+#   # Configure network proxy if necessary
+#   # networking.proxy.default = "http://user:password@proxy:port/";
+#   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+# 
+#   # Enable networking
+#   networking.networkmanager.enable = true;
+# 
+#   # Set your time zone.
+#   time.timeZone = "America/New_York";
+# 
+#   # Select internationalisation properties.
+#   i18n.defaultLocale = "en_US.UTF-8";
+# 
+#   i18n.extraLocaleSettings = {
+#     LC_ADDRESS = "en_US.UTF-8";
+#     LC_IDENTIFICATION = "en_US.UTF-8";
+#     LC_MEASUREMENT = "en_US.UTF-8";
+#     LC_MONETARY = "en_US.UTF-8";
+#     LC_NAME = "en_US.UTF-8";
+#     LC_NUMERIC = "en_US.UTF-8";
+#     LC_PAPER = "en_US.UTF-8";
+#     LC_TELEPHONE = "en_US.UTF-8";
+#     LC_TIME = "en_US.UTF-8";
+#   };
+# 
+#   # Enable the X11 windowing system.
+#   services.xserver.enable = true;
+# 
+#   # Enable the GNOME Desktop Environment.
+#   services.xserver.displayManager.gdm.enable = true;
+#   services.xserver.desktopManager.gnome.enable = true;
+# 
+#   # Configure keymap in X11
+#   services.xserver = {
+#     layout = "us";
+#     xkbVariant = "";
+#   };
+# 
+#   # Enable CUPS to print documents.
+#   services.printing.enable = true;
+# 
+#   # Enable sound with pipewire.
+#   hardware.pulseaudio.enable = false;
+#   security.rtkit.enable = true;
+#   services.pipewire = {
+#     enable = true;
+#     alsa.enable = true;
+#     alsa.support32Bit = true;
+#     pulse.enable = true;
+#     # If you want to use JACK applications, uncomment this
+#     #jack.enable = true;
+# 
+#     # use the example session manager (no others are packaged yet so this is enabled by default,
+#     # no need to redefine it in your config for now)
+#     #media-session.enable = true;
+#   };
+# 
+#   # Enable touchpad support (enabled default in most desktopManager).
+#   # services.xserver.libinput.enable = true;
+# 
+#   # Define a user account. Don't forget to set a password with ‘passwd’.
+#   users.users.mitch = {
+#     isNormalUser = true;
+#     description = "mitch";
+#     extraGroups = [ "networkmanager" "wheel" ];
+#     packages = with pkgs; [
+#     #  thunderbird
+#     ];
+#   };
+# 
+#   # Install firefox.
+#   programs.firefox.enable = true;
+# 
+#   # Allow unfree packages
+#   nixpkgs.config.allowUnfree = true;
+# 
+#   # List packages installed in system profile. To search, run:
+#   # $ nix search wget
+#   environment.systemPackages = with pkgs; [
+#     neovim
+#     wget
+#     less
+#     man
+#     git
+#   ];
+# 
+#   # Some programs need SUID wrappers, can be configured further or are
+#   # started in user sessions.
+#   # programs.mtr.enable = true;
+#   # programs.gnupg.agent = {
+#   #   enable = true;
+#   #   enableSSHSupport = true;
+#   # };
+# 
+#   # List services that you want to enable:
+# 
+#   # Enable the OpenSSH daemon.
+#   # services.openssh.enable = true;
+# 
+#   # Open ports in the firewall.
+#   # networking.firewall.allowedTCPPorts = [ ... ];
+#   # networking.firewall.allowedUDPPorts = [ ... ];
+#   # Or disable the firewall altogether.
+#   # networking.firewall.enable = false;
+# 
+#   # This value determines the NixOS release from which the default
+#   # settings for stateful data, like file locations and database versions
+#   # on your system were taken. It‘s perfectly fine and recommended to leave
+#   # this value at the release version of the first install of this system.
+#   # Before changing this value read the documentation for this option
+#   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+#   system.stateVersion = "24.05"; # Did you read the comment?
+# 
+# }
