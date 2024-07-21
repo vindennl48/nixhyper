9d38e11d71473b5bd909b87a73fb8c2fe65473e1
Mitchell Woodbine <mitchellwoodbine@gmail.com>
2024-07-21 18:26:31 -0400

    Added bridged network to nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -185,6 +185,8 @@
   networking = {
     # Enable networking
     networkmanager.enable = true;
+    networkmanager.unmanaged = [ "br0" ];
+
     # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     hostName = "nixos"; # Define your hostname.
 
@@ -196,6 +198,36 @@
     firewall.allowedTCPPorts = [ 22 8000 5901 ];
     # firewall.allowedUDPPorts = [ ... ];
     # firewall.enable = false; # disable the firewall altogether.
+  };
+
+  systemd.network = {
+    enable = true;
+    wait-online.enable = false;
+    netdevs = {
+      "1-br0" = {
+        enable = true;
+        netdevConfig = {
+          Name = "br0";
+          Kind = "bridge";
+        };
+      };
+    };
+    networks = {
+      "2-br0-bind" = {
+        matchConfig.Name = "enp6s0";
+        networkConfig.Bridge = "br0";
+      };
+      "3-br0-dhcp" = {
+        matchConfig.Name = "br0";
+        networkConfig.DHCP = "ipv4";
+      };
+      "20-wlan" = {
+        matchConfig.Name = "wl*";
+        linkConfig.RequiredForOnline = "routable";
+        networkConfig.DHCP = "yes";
+        networkConfig.MulticastDNS = "yes";
+      };
+    };
   };
 
 ################################################################################
@@ -292,137 +324,3 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 }
-
-# # Edit this configuration file to define what should be installed on
-# # your system.  Help is available in the configuration.nix(5) man page
-# # and in the NixOS manual (accessible by running ‘nixos-help’).
-# 
-# { config, pkgs, ... }:
-# 
-# {
-#   imports =
-#     [ # Include the results of the hardware scan.
-#       ./hardware-configuration.nix
-#     ];
-# 
-#   # Bootloader.
-#   boot.loader.systemd-boot.enable = true;
-#   boot.loader.efi.canTouchEfiVariables = true;
-# 
-#   networking.hostName = "nixos"; # Define your hostname.
-#   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-# 
-#   # Configure network proxy if necessary
-#   # networking.proxy.default = "http://user:password@proxy:port/";
-#   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-# 
-#   # Enable networking
-#   networking.networkmanager.enable = true;
-# 
-#   # Set your time zone.
-#   time.timeZone = "America/New_York";
-# 
-#   # Select internationalisation properties.
-#   i18n.defaultLocale = "en_US.UTF-8";
-# 
-#   i18n.extraLocaleSettings = {
-#     LC_ADDRESS = "en_US.UTF-8";
-#     LC_IDENTIFICATION = "en_US.UTF-8";
-#     LC_MEASUREMENT = "en_US.UTF-8";
-#     LC_MONETARY = "en_US.UTF-8";
-#     LC_NAME = "en_US.UTF-8";
-#     LC_NUMERIC = "en_US.UTF-8";
-#     LC_PAPER = "en_US.UTF-8";
-#     LC_TELEPHONE = "en_US.UTF-8";
-#     LC_TIME = "en_US.UTF-8";
-#   };
-# 
-#   # Enable the X11 windowing system.
-#   services.xserver.enable = true;
-# 
-#   # Enable the GNOME Desktop Environment.
-#   services.xserver.displayManager.gdm.enable = true;
-#   services.xserver.desktopManager.gnome.enable = true;
-# 
-#   # Configure keymap in X11
-#   services.xserver = {
-#     layout = "us";
-#     xkbVariant = "";
-#   };
-# 
-#   # Enable CUPS to print documents.
-#   services.printing.enable = true;
-# 
-#   # Enable sound with pipewire.
-#   hardware.pulseaudio.enable = false;
-#   security.rtkit.enable = true;
-#   services.pipewire = {
-#     enable = true;
-#     alsa.enable = true;
-#     alsa.support32Bit = true;
-#     pulse.enable = true;
-#     # If you want to use JACK applications, uncomment this
-#     #jack.enable = true;
-# 
-#     # use the example session manager (no others are packaged yet so this is enabled by default,
-#     # no need to redefine it in your config for now)
-#     #media-session.enable = true;
-#   };
-# 
-#   # Enable touchpad support (enabled default in most desktopManager).
-#   # services.xserver.libinput.enable = true;
-# 
-#   # Define a user account. Don't forget to set a password with ‘passwd’.
-#   users.users.mitch = {
-#     isNormalUser = true;
-#     description = "mitch";
-#     extraGroups = [ "networkmanager" "wheel" ];
-#     packages = with pkgs; [
-#     #  thunderbird
-#     ];
-#   };
-# 
-#   # Install firefox.
-#   programs.firefox.enable = true;
-# 
-#   # Allow unfree packages
-#   nixpkgs.config.allowUnfree = true;
-# 
-#   # List packages installed in system profile. To search, run:
-#   # $ nix search wget
-#   environment.systemPackages = with pkgs; [
-#     neovim
-#     wget
-#     less
-#     man
-#     git
-#   ];
-# 
-#   # Some programs need SUID wrappers, can be configured further or are
-#   # started in user sessions.
-#   # programs.mtr.enable = true;
-#   # programs.gnupg.agent = {
-#   #   enable = true;
-#   #   enableSSHSupport = true;
-#   # };
-# 
-#   # List services that you want to enable:
-# 
-#   # Enable the OpenSSH daemon.
-#   # services.openssh.enable = true;
-# 
-#   # Open ports in the firewall.
-#   # networking.firewall.allowedTCPPorts = [ ... ];
-#   # networking.firewall.allowedUDPPorts = [ ... ];
-#   # Or disable the firewall altogether.
-#   # networking.firewall.enable = false;
-# 
-#   # This value determines the NixOS release from which the default
-#   # settings for stateful data, like file locations and database versions
-#   # on your system were taken. It‘s perfectly fine and recommended to leave
-#   # this value at the release version of the first install of this system.
-#   # Before changing this value read the documentation for this option
-#   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-#   system.stateVersion = "24.05"; # Did you read the comment?
-# 
-# }
